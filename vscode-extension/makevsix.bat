b68c52f614db700a8061805be538ef411bafc258
Florian Rabe <florian.rabe@gmail.com>
2025-01-11 02:54:07 +0100

    no message

---
+++ vscode-extension/makevsix.bat
@@ -0,0 +1,7 @@
+:: as compiled by sbt fastLinkJS
+copy /Y ..\target\scala-2.13\upl-fastopt\* extension\src
+:: workaround: Scala.js apparently cannot generate a js file that both Node and browsers like
+:: here: add the export declaration that Node wants to have
+echo module.exports = {VSCodeBridge}; >> extension\src\main.js
+:: build the VSCode extension package (excluding this script)
+zip -r upl-vscode.vsix * -x makevsix.bat
