77c5933ab97c64a0b4fda8948b3b00be2be59f89
henryboehm <henryboehm98@gmail.com>
2025-09-13 17:11:13 +0200

    Haskell functor

--- test/categories/formalizations/categories.pp
+++ test/categories/formalizations/categories.pp
@@ -1,2 +1,2 @@
-source: ./category1st.p ./category2st.p ./monoid.p ./functor1st.p ./category3dt.p
+source: ./category1st.p ./category2st.p ./monoid.p ./functor1st.p ./functorHaskell.p
 main: CatST.test
