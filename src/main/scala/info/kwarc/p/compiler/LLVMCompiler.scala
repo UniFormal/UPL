a96d11397af38d23cb834eb159028133c73ef6e3
Drex <nicknamedrex@gmail.com>
2026-05-11 20:56:27 +0200

    Compile to binary

--- src/main/scala/info/kwarc/p/compiler/LLVMCompiler.scala
+++ src/main/scala/info/kwarc/p/compiler/LLVMCompiler.scala
@@ -2,10 +2,27 @@ package info.kwarc.p.compiler
 
 import info.kwarc.p.Program
 
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+
 object LLVMCompiler {
   def run(p: Program) = {
     val cp = new LLVMCompiler()
-    println(cp.compile(p))
+
+    val pb = new ProcessBuilder(
+      "clang",
+      "-x", "ir",
+      "-",
+      "-o", "out"
+    )
+    pb.redirectErrorStream(true)
+    val process = pb.start
+
+    val writer = new OutputStreamWriter(process.getOutputStream, StandardCharsets.UTF_8)
+    try {
+      writer.write(cp.compile(p))
+    } finally writer.close()
+    process.waitFor()
     cp
   }
 }
